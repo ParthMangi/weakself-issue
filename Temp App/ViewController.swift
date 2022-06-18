aae04d8c1406528e40c784366c5110988881e5e5
Parth Mangi <parthmangi.ios@gmail.com>
2022-06-18 14:55:02 +0530

    Initial Commit

---
+++ Temp App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Temp App
+//
+//  Created by Parth Mangi on 18/06/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
